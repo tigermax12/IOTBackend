2611fa86f6db73ce00d80730bee3ee5dcf400737
DAW208 <daw08.2024@gmail.com>
2025-04-07 12:16:01 +0200

    Bases

--- database/migrations/2025_04_07_100143_create_data_table.php
+++ database/migrations/2025_04_07_100143_create_data_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('data', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['Administrador', 'Profesor', 'Usuario']);
+            $table->timestamp('timestamp');
+            $table->string('topic');
+            $table->double('value');
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('data');
     }
 };
--- database/migrations/2025_04_07_100333_create_notifications_table.php
+++ database/migrations/2025_04_07_100333_create_notifications_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['Administrador', 'Profesor', 'Usuario']);
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('message');
+            $table->boolean('read')->default(false);
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('notifications');
     }
 };
